d89b127cadac45c28ebcb9a8d885e73e1c17fe2b
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-12-12 07:51:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Enterprise;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
