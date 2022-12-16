7990ed14c57e4864d05dcc10144ae8b56ed281e1
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-12-15 22:03:38 -0300

    Criando a tela de cadastro de cliente

---
+++ PageModel/Clientes/ClienteConsultaPageModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Enterprise.PageModel.Clientes
+{
+    public class ClienteConsultaPageModel
+    {
+        public Guid Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }
+        public string CNPJ { get; set; }
+        public string Site { get; set; }
+    }
+}
---
+++ PageModel/Clientes/ClienteEdicaoPageModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Enterprise.PageModel.Clientes
+{
+    public class ClienteEdicaoPageModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string RazaoSocial { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string NomeFantasia { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string CNPJ { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Site { get; set; }
+    }
+}
